a8347e4e34e4094afa95a0a68621cbdd3b083730
Dima Nagorniy <dima.nagorniy@gmail.com>
2016-04-27 12:09:05 +0300

    Replace array with Columns object in some places.

--- src/MySql/Sql/CreateAuditTable.php
+++ src/MySql/Sql/CreateAuditTable.php
@@ -3,6 +3,8 @@
 namespace SetBased\Audit\MySql\Sql;
 
 //----------------------------------------------------------------------------------------------------------------------
+use SetBased\Audit\Columns;
+
 /**
  * Class for creating and executing SQL statements for creating audit tables.
  */
@@ -19,7 +21,7 @@ class CreateAuditTable
   /**
    * The name of the table.
    *
-   * @var array[]
+   * @var Columns
    */
   private $columns;
 
@@ -36,7 +38,7 @@ class CreateAuditTable
    *
    * @param string  $auditSchemaName The name of the audit schema.
    * @param string  $tableName       The name of the table.
-   * @param array[] $columns         The metadata of the columns of the audit table (i.e. the audit columns and columns
+   * @param Columns $columns         The metadata of the columns of the audit table (i.e. the audit columns and columns
    *                                 of the data table).
    */
   public function __construct($auditSchemaName,
@@ -57,10 +59,10 @@ class CreateAuditTable
   public function buildStatement()
   {
     $sql = sprintf('create table `%s`.`%s`(', $this->auditSchemaName, $this->tableName);
-    foreach ($this->columns as $column)
+    foreach ($this->columns->getColumns() as $column)
     {
       $sql .= sprintf('`%s` %s', $column['column_name'], $column['column_type']);
-      if (end($this->columns)!==$column)
+      if (end($this->columns->getColumns())!==$column)
       {
         $sql .= ',';
       }
